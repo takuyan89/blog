607f6c7d6683181b08611622b449c0a2939afef9
Takuya Nomura <git.takuya89@gmail.com>
2025-06-15 17:20:34 +0200

    auth.jsの設定

--- src/app/(public)/posts/[id]/page.tsx
+++ src/app/(public)/posts/[id]/page.tsx
@@ -22,19 +22,19 @@ export default async function PostPage({ params }: Params) {
   return (
     <div className='container mx-auto px-4 py-8'>
       <Card className='max-w-3xl mx-auto'>
-        {post.topImage && (
-          <div className='relative w-full h-64 lg:h-96'>
-            <Image
-              src={post.topImage}
-              alt={post.title}
-              fill
-              sizes='100vw'
-              className='rounded-t-md object-cover'
-              priority
-            />
-          </div>
-        )}
         <CardHeader>
+          {post.topImage && (
+            <div className='relative w-full h-64 lg:h-96'>
+              <Image
+                src={post.topImage}
+                alt={post.title}
+                fill
+                sizes='100vw'
+                className='rounded-t-md object-cover'
+                priority
+              />
+            </div>
+          )}
           <div className='flex items-center justify-between mb-4'>
             <p className='text-sm text-gray-500'>投稿者：{post.author.name}</p>
             <time className='text-sm text-gray-500'>
